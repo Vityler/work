7ade9237308aa89867de46f0fdffe69ee651c354
Vitaliy <krvc.vitaliy@gmail.com>
2021-11-25 17:27:46 +0200

    add comments

---
+++ js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+//alert('Hello world');//всплывающее окно в браузере
+
+//const result = confirm("Are you here"); окно с вариантами "да" или "нет"
+//console.log(result);
+
+//const answer = +prompt('Вам есть 18?',"18");
+//console.log(answer+5);
+
+const answers = [];
+answers[0]=prompt('bhfgbdf','');
+answers[1]=prompt('ayayy','');
+answers[2]=prompt('hey','');
+
+document.write(answers);
---
+++ learn/типы данных.js
@@ -0,0 +1,26 @@
+"use strict";
+
+let number = 4;
+
+console.log(4/0);       //числа, кавычки не нцжны
+console.log('string'*8);
+
+const person = "alex";//строки в кавычки, " или '
+
+const bool = true;
+
+console.log(something);//null
+
+let und;
+console.log(und);//udefined
+
+const obj ={//объект, имеет ключ и значение
+    name:'John',
+    age:25,
+    isMarried:false
+};
+//console.log(obj.name);
+console.log(obj['name']);
+
+let arr = ['plum.png', 'orange.jpg',6,[], {}, 'apple.bmp'];// массив, ключ - порядковый номер от 0
+console.log(arr[2]);//значение в квадратных скобках
